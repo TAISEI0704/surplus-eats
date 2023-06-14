e5e085af37b5a3f7da46deba47d39c57826a7a28
abe113 <youheiabe0711@gmail.com>
2023-06-14 15:54:31 +0800

    背景色（仮）

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -1,4 +1,4 @@
-<nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
+<nav x-data="{ open: false }" style="background-color: #F8F7EE" class="border-b border-gray-100">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
@@ -6,11 +6,22 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ Auth::user()->is_seller ? route('seller.dashboard') : route('dashboard') }}">
-                        <x-jet-application-mark class="block h-9 w-auto" />
+                        {{-- <x-jet-application-mark class="block h-9 w-auto" /> --}}
+                        <img src="{{ asset('img/logo.png') }}" alt="" class="block h-20 w-auto">
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-jet-nav-link href="{{ Auth::user()->is_seller ? route('seller.dashboard') : route('dashboard') }}" :active="request()->routeIs('dashboard')">
+                        {{ __('Catalog page') }}
+                    </x-jet-nav-link>
+                </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-jet-nav-link href="{{ Auth::user()->is_seller ? route('seller.dashboard') : route('dashboard') }}" :active="request()->routeIs('dashboard')">
+                        {{ __('Dashboard') }}
+                    </x-jet-nav-link>
+                </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-jet-nav-link href="{{ Auth::user()->is_seller ? route('seller.dashboard') : route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
